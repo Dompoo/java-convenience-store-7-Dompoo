a768bd5e104cbddcd170223edfeebf577397a82e
이창근 <dlckdrms0517@gmail.com>
2024-11-11 01:47:26 +0900

    feat: Cashier 작성
    - Cashier는 외부 결정에 따라 의사결정 타입을 구매 타입으로 변환합니다.

---
+++ src/main/java/store/domain/Cashier.java
@@ -0,0 +1,52 @@
+package store.domain;
+
+final public class Cashier {
+
+    public static PurchaseType decidePurchaseType(
+            final Product product,
+            final int purchaseAmount,
+            final DecisionType decisionType,
+            final DecisionSupplier<Boolean> bringFreeProductSupplier,
+            final DecisionSupplier<Boolean> bringDefaultProductBackSupplier
+    ) {
+        if (decisionType == DecisionType.FULL_DEFAULT) {
+            return PurchaseType.FULL_DEFAULT;
+        }
+        if (decisionType == DecisionType.FULL_PROMOTION) {
+            return PurchaseType.FULL_PROMOTION;
+        }
+        if (decisionType == DecisionType.CAN_GET_FREE_PRODUCT) {
+            return decideBringFreeProduct(product, purchaseAmount, bringFreeProductSupplier);
+        }
+        return decideBringDefaultProductBack(product, purchaseAmount, bringDefaultProductBackSupplier);
+    }
+
+    private static PurchaseType decideBringFreeProduct(
+            final Product product,
+            final int purchaseAmount,
+            final DecisionSupplier<Boolean> bringFreeProductPredicate
+    ) {
+        if (bringFreeProductPredicate.get(product.getName(), product.calculateBringFreeProductCount(purchaseAmount))) {
+            return PurchaseType.FULL_PROMOTION_BRING_FREE;
+        }
+        return PurchaseType.FULL_PROMOTION_NOT_BRING_FREE;
+    }
+
+    private static PurchaseType decideBringDefaultProductBack(
+            final Product product,
+            final int purchaseAmount,
+            final DecisionSupplier<Boolean> bringDefaultProductBackPredicate
+    ) {
+        if (bringDefaultProductBackPredicate.get(
+                product.getName(),
+                product.calculateNoPromotionsProductCount(purchaseAmount))) {
+            return PurchaseType.PORTION_PROMOTION_NOT_BRING_BACK;
+        }
+        return PurchaseType.PORTION_PROMOTION_BRING_BACK;
+    }
+
+    @FunctionalInterface
+    public interface DecisionSupplier<T> {
+        T get(String name, Integer count);
+    }
+}
