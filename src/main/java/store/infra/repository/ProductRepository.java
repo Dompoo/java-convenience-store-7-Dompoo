8e65e2506926002e0dcf5b6b3c594fa15f3287e4
이창근 <dlckdrms0517@gmail.com>
2024-11-11 02:32:10 +0900

    feat: ProductRepository 작성
    - 실제로 서비스에서 호출하게 되는 의존성입니다.
    - 생성시에 상품을 불러와서 가지고 있다가, 한번에 파일에 작성합니다.

---
+++ src/main/java/store/infra/repository/ProductRepository.java
@@ -0,0 +1,84 @@
+package store.infra.repository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import store.common.exception.StoreExceptions;
+import store.domain.Product;
+import store.infra.database.Database;
+import store.infra.entity.ProductEntity;
+import store.infra.entity.PromotionEntity;
+import store.infra.repository.converter.ProductConverter;
+import store.infra.repository.converter.ProductEntityConverter;
+import store.infra.repository.converter.PromotionEntityConverter;
+import store.service.dateProvider.DateProvider;
+
+public class ProductRepository implements Repository<Product> {
+
+    private final Database<ProductEntity> productDatabase;
+    private final Database<PromotionEntity> promotionDatabase;
+    private final ProductEntityConverter productEntityConverter;
+    private final PromotionEntityConverter promotionEntityConverter;
+
+    private final List<Product> products = new ArrayList<>();
+
+    public ProductRepository(
+            final Database<ProductEntity> productDatabase,
+            final Database<PromotionEntity> promotionDatabase,
+            final ProductConverter productConverter,
+            final ProductEntityConverter productEntityConverter,
+            final PromotionEntityConverter promotionEntityConverter,
+            final DateProvider dateProvider
+    ) {
+        this.productEntityConverter = productEntityConverter;
+        this.promotionEntityConverter = promotionEntityConverter;
+        this.productDatabase = productDatabase;
+        this.promotionDatabase = promotionDatabase;
+        List<PromotionEntity> promotionEntities = getUniquePromotionEntities(promotionDatabase, dateProvider.getDate());
+        this.products.addAll(
+                productConverter.convert(productDatabase.readAll(), promotionEntities, dateProvider.getDate())
+        );
+    }
+
+    private static List<PromotionEntity> getUniquePromotionEntities(
+            final Database<PromotionEntity> promotionDatabase,
+            final LocalDate now
+    ) {
+        return promotionDatabase.readAll().stream()
+                .filter(promotionEntity -> promotionEntity.isAvailable(now))
+                .toList();
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return products;
+    }
+
+    @Override
+    public Optional<Product> findByName(final String name) {
+        for (Product product : products) {
+            if (product.getName().equals(name)) {
+                return Optional.of(product);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(final Product product) {
+        Product findProduct = products.stream()
+                .filter(products -> products.getName().equals(product.getName()))
+                .findFirst()
+                .orElseThrow(StoreExceptions.PRODUCT_NOT_FOUND::get);
+
+        products.remove(findProduct);
+        products.add(product);
+    }
+
+    @Override
+    public void updateDatabaseInBatch() {
+        productDatabase.updateAll(productEntityConverter.convert(products));
+        promotionDatabase.updateAll(promotionEntityConverter.convert(products));
+    }
+}
---
+++ src/test/java/store/infra/repository/ProductRepositoryTest.java
@@ -0,0 +1,144 @@
+package store.infra.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import store.domain.Product;
+import store.domain.Promotion;
+import store.domain.PromotionType;
+import store.domain.Stock;
+import store.infra.entity.ProductEntity;
+import store.infra.entity.PromotionEntity;
+import store.infra.repository.converter.ProductConverter;
+import store.infra.repository.converter.ProductEntityConverter;
+import store.infra.repository.converter.PromotionEntityConverter;
+import store.testUtil.testDouble.DateProviderStub;
+import store.testUtil.testDouble.ProductFileDatabaseFake;
+import store.testUtil.testDouble.PromotionFileDatabaseFake;
+
+class ProductRepositoryTest {
+
+    private final LocalDate pastDate = LocalDate.now().minusDays(10);
+    private final LocalDate now = LocalDate.now();
+    private final LocalDate futureDate = LocalDate.now().plusDays(10);
+    private ProductRepository sut;
+
+    @BeforeEach
+    void setUp() {
+        ProductFileDatabaseFake productFileDatabaseFake = new ProductFileDatabaseFake();
+        PromotionFileDatabaseFake promotionFileDatabaseFake = new PromotionFileDatabaseFake();
+        setUpProductFileDatebaseFake(productFileDatabaseFake);
+        setUpPromotionFileDatabaseFake(promotionFileDatabaseFake);
+        DateProviderStub dateProviderStub = new DateProviderStub();
+        dateProviderStub.setDate(now);
+        sut = new ProductRepository(
+                productFileDatabaseFake,
+                promotionFileDatabaseFake,
+                new ProductConverter(),
+                new ProductEntityConverter(),
+                new PromotionEntityConverter(),
+                dateProviderStub
+        );
+    }
+
+    private static void setUpProductFileDatebaseFake(ProductFileDatabaseFake productFileDatabaseFake) {
+        productFileDatabaseFake.setUpProductEntities(List.of(
+                new ProductEntity("콜라", 1000, 5, ""),
+                new ProductEntity("콜라", 1000, 5, "콜라1+1"),
+                new ProductEntity("감자", 1500, 10, ""),
+                new ProductEntity("땅콩", 1000, 3, ""),
+                new ProductEntity("땅콩", 1000, 3, "과거프로모션"),
+                new ProductEntity("땅콩버터", 2000, 5, "과거프로모션")
+        ));
+    }
+
+    private void setUpPromotionFileDatabaseFake(PromotionFileDatabaseFake promotionFileDatabaseFake) {
+        promotionFileDatabaseFake.setPromotionEntities(List.of(
+                new PromotionEntity("콜라1+1", 1, 1, pastDate, futureDate),
+                new PromotionEntity("초코바2+1", 2, 1, pastDate, futureDate),
+                new PromotionEntity("과거프로모션", 1, 1, pastDate, pastDate)
+        ));
+    }
+
+    @Nested
+    class 모든_상품_읽기_테스트 {
+
+        @Test
+        void 모든_상품을_읽는다() {
+            //given
+
+            //when
+            List<Product> result = sut.findAll();
+
+            //then
+            assertThat(result).extracting(
+                    "name", "price", "stock"
+            ).containsExactlyInAnyOrder(
+                    Tuple.tuple("콜라", 1000, new Stock(5, 5)),
+                    Tuple.tuple("감자", 1500, new Stock(10, 0)),
+                    Tuple.tuple("땅콩", 1000, new Stock(3, 0))
+            );
+        }
+    }
+
+    @Nested
+    class 이름으로_상품_읽기_테스트 {
+
+        @Test
+        void 상품_이름으로_읽는다() {
+            //given
+            String productName = "콜라";
+
+            //when
+            Optional<Product> result = sut.findByName(productName);
+
+            //then
+            assertThat(result).isPresent();
+            assertThat(result.get()).extracting(
+                    "name", "price", "stock"
+            ).containsExactly(
+                    "콜라", 1000, new Stock(5, 5)
+            );
+        }
+
+        @Test
+        void 존재하지_않는_상품_이름으로_읽는다() {
+            //given
+            String productName = "없는상품이지롱";
+
+            //when
+            Optional<Product> result = sut.findByName(productName);
+
+            //then
+            assertThat(result).isEmpty();
+        }
+    }
+
+    @Nested
+    class 상품_업데이트_테스트 {
+
+        @Test
+        void 새로운_상품을_업데이트한다() {
+            //given
+            Promotion promotion = new Promotion("새로운상품프로모션", PromotionType.BUY_ONE_GET_ONE, pastDate, futureDate);
+            Product product = new Product("콜라", 1500, 10, 10, promotion);
+
+            //when
+            sut.update(product);
+
+            //then
+            assertThat(sut.findByName("콜라")).isPresent();
+            assertThat(sut.findByName("콜라").get()).extracting(
+                    "name", "price", "stock", "promotion.name", "promotion.promotionType"
+            ).containsExactly(
+                    "콜라", 1500, new Stock(10, 10), "새로운상품프로모션", PromotionType.BUY_ONE_GET_ONE
+            );
+        }
+    }
+}
