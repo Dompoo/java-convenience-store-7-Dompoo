ad8475636adcbd5646a9388d880b99e0b1fd5bd9
이창근 <dlckdrms0517@gmail.com>
2024-11-11 02:40:41 +0900

    feat: DecisionService 작성

---
+++ src/main/java/store/service/decisionService/DecisionService.java
@@ -0,0 +1,24 @@
+package store.service.decisionService;
+
+import java.util.function.Supplier;
+import store.common.dto.request.PurchaseRequest;
+import store.domain.Cashier.DecisionSupplier;
+import store.domain.DecisionType;
+import store.domain.PurchaseType;
+import store.domain.membership.Membership;
+
+public interface DecisionService {
+
+    DecisionType getDecisionType(PurchaseRequest purchaseRequest);
+
+    PurchaseType decidePurchaseType(
+            PurchaseRequest purchaseRequest,
+            DecisionType decisionType,
+            DecisionSupplier<Boolean> bringFreeProductDecisionSupplier,
+            DecisionSupplier<Boolean> bringDefaultProductBackDecisionSupplier
+    );
+
+    Membership decideMembership(
+            Supplier<Boolean> membershipDecisionSupplier
+    );
+}
---
+++ src/main/java/store/service/decisionService/DefaultDecisionService.java
@@ -0,0 +1,58 @@
+package store.service.decisionService;
+
+import java.util.function.Supplier;
+import store.common.dto.request.PurchaseRequest;
+import store.common.exception.StoreExceptions;
+import store.domain.Cashier;
+import store.domain.Cashier.DecisionSupplier;
+import store.domain.DecisionType;
+import store.domain.Product;
+import store.domain.PurchaseType;
+import store.domain.membership.Membership;
+import store.domain.membership.NoMembership;
+import store.domain.membership.RatioMembership;
+import store.infra.repository.Repository;
+
+public class DefaultDecisionService implements DecisionService {
+
+    private final Repository<Product> productRepository;
+
+    public DefaultDecisionService(
+            final Repository<Product> productRepository
+    ) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public DecisionType getDecisionType(final PurchaseRequest purchaseRequest) {
+        Product product = productRepository.findByName(purchaseRequest.productName())
+                .orElseThrow(StoreExceptions.PRODUCT_NOT_FOUND::get);
+        return DecisionType.of(product, purchaseRequest.purchaseAmount());
+    }
+
+    @Override
+    public PurchaseType decidePurchaseType(
+            final PurchaseRequest purchaseRequest,
+            final DecisionType decisionType,
+            final DecisionSupplier<Boolean> bringFreeProductDecisionSupplier,
+            final DecisionSupplier<Boolean> bringDefaultProductBackDecisionSupplier
+    ) {
+        Product product = productRepository.findByName(purchaseRequest.productName())
+                .orElseThrow(StoreExceptions.PRODUCT_NOT_FOUND::get);
+        return Cashier.decidePurchaseType(
+                product,
+                purchaseRequest.purchaseAmount(),
+                decisionType,
+                bringFreeProductDecisionSupplier,
+                bringDefaultProductBackDecisionSupplier
+        );
+    }
+
+    @Override
+    public Membership decideMembership(final Supplier<Boolean> membershipDecisionSupplier) {
+        if (membershipDecisionSupplier.get()) {
+            return new RatioMembership();
+        }
+        return new NoMembership();
+    }
+}
